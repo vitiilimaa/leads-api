9605ee3276812b12087a0bfe5589ff6dbde4327b
Vitor Batista <vitiilimaa@gmail.com>
2025-09-29 08:32:19 -0300

    fix: implementando next() do middleware de erros da aplicação

--- src/middlewares/error-middleware.ts
+++ src/middlewares/error-middleware.ts
@@ -2,11 +2,15 @@ import { ErrorRequestHandler } from "express";
 import { HttpError } from "../errors/HttpError";
 
 export const errorMiddleware: ErrorRequestHandler = (error, req, res, next) => {
-  if (error instanceof HttpError) {
-    res.status(error.status).json({ message: error.message });
-  } else if (error instanceof Error) {
-    res.status(500).json({ message: error.message });
+  if (error) {
+    if (error instanceof HttpError) {
+      res.status(error.status).json({ message: error.message });
+    } else if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Erro não identificado no servidor." });
+    }
   } else {
-    res.status(500).json({ message: "Erro não identificado no servidor." });
+    next();
   }
 };
